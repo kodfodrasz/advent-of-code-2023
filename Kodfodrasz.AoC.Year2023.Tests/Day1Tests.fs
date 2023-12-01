4ed5791ac5a5cf1712d76c3a5c88f6d5fb71eadf
Gábor Gergely <ggergely@kodfodrasz.net>
2023-12-01 23:08:25 +0100

    Day 1, both parts

--- Kodfodrasz.AoC.Year2023.Tests/Day1Tests.fs
+++ Kodfodrasz.AoC.Year2023.Tests/Day1Tests.fs
@@ -8,45 +8,19 @@ open Kodfodrasz.AoC.Year2023.Day1
 
 
 let exampleInput = """
-1000
-2000
-3000
-
-4000
-
-5000
-6000
-
-7000
-8000
-9000
-
-10000
+1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
   """
 
 [<Fact>]
 let ``Parsing example input`` () =
-  let expected: Result<int list list, string> = Ok [ 
-    [
-      1000
-      2000
-      3000
-    ];
-    [
-      4000
-    ];
-    [
-      5000
-      6000
-    ];
-    [
-      7000
-      8000
-      9000
-    ];
-    [
-      10000
-    ]
+  let expected: Result<string list, string> = Ok [ 
+    "1abc2";
+    "pqr3stu8vwx";
+    "a1b2c3d4e5f";
+    "treb7uchet";
   ]
 
   test
@@ -59,14 +33,39 @@ let ``Answer 1 for example input`` () =
 
   test
     <@ let actual = Result.bind answer1 input
-       let expected: Result<int, string> = Ok 24000
+       let expected: Result<int, string> = Ok 142
        actual = expected @>
 
 [<Fact>]
-let ``Answer 2 for example input`` () =
-  let input = parseInput exampleInput
+let ``Helper for Answer 2`` () =
+  let expected = [ 
+    "abcd";
+    "bcd";
+    "cd";
+    "d";
+  ]
+
+  test 
+    <@
+      let actual = Day1.substrings "abcd"
+      actual = expected
+    @>
+
+let exampleInput2 = """
+    two1nine
+    eightwothree
+    abcone2threexyz
+    xtwone3four
+    4nineeightseven2
+    zoneight234
+    7pqrstsixteen
+    """
+
+[<Fact>]
+let ``Answer 2 for example input 2`` () =
+  let input = parseInput exampleInput2
 
   test
     <@ let actual = Result.bind answer2 input
-       let expected: Result<int, string> = Ok 45000
+       let expected: Result<int, string> = Ok 281
        actual = expected @>
--- Kodfodrasz.AoC.Year2023/Day1.fs
+++ Kodfodrasz.AoC.Year2023/Day1.fs
@@ -1,18 +1,17 @@
 module Kodfodrasz.AoC.Year2023.Day1
 
+open System
 open Kodfodrasz.AoC
 
 
-let parseInput (input: string): Result<int list list, string> =
+let parseInput (input: string): Result<string list, string> =
   input.Split('\n')
   |> Seq.map String.trim
   |> Seq.skipWhile String.isNullOrEmpty
-  |> Seq.batchBySeparator ""
-  |> Seq.map (List.map (fun line -> Parse.parseInt line))
-  |> Seq.map (List.choose id)
+  // no transformation
+  |> Seq.where String.notNullOrWhiteSpace
   |> Seq.toList
-  |> Ok
-
+  |> Ok 
 
   //let errorMaybe =
   //  Seq.tryFind (fun (_, numMaybe) -> Option.isNone numMaybe) numbersMaybe
@@ -27,22 +26,94 @@ let parseInput (input: string): Result<int list list, string> =
   //    sprintf "Input line could not be parsed to integer: %s" line
   //    |> Error
 
-let answer1 (numbers : int list list) =
+let answer1 (strings: string list) =
+  let numbers = 
+    strings
+    |> Seq.map (fun (s:string) -> 
+        s.ToCharArray() 
+        |> Seq.where Char.IsDigit 
+        |> Seq.map (string >> Int32.Parse)
+        |> (fun i -> 10 * Seq.head i + Seq.last i))
+    |> Seq.toArray
+
   numbers
-  |> List.map List.sum
-  |> List.max
+  |> Seq.sum
   |> Ok
 
-let answer2 numbers =
+let substrings s = 
+  let rec inner_substrings (acc:string list) s = 
+      match s with
+      | "" -> acc |> List.rev
+      | _ -> inner_substrings (s :: acc) (s.Substring(1))
+
+  inner_substrings [] s
+
+let evilLigatureParser line =
+  let parseStart = fun (s:string) ->
+    match s with
+    | s  when s.StartsWith "one" -> Some 1
+    | s  when s.StartsWith "two" -> Some 2
+    | s  when s.StartsWith "three" -> Some 3
+    | s  when s.StartsWith "four" -> Some 4
+    | s  when s.StartsWith "five" -> Some 5
+    | s  when s.StartsWith "six" -> Some 6
+    | s  when s.StartsWith "seven" -> Some 7
+    | s  when s.StartsWith "eight" -> Some 8
+    | s  when s.StartsWith "nine" -> Some 9
+    | s  when (s.ToCharArray() |> Seq.tryHead |> Option.map Char.IsDigit |> Option.defaultValue false)
+      -> s.Substring(0,1) |> Int32.Parse |> Some
+    | _ -> None
+
+  substrings line
+  |> List.map parseStart
+  |> List.choose id
+  |> (fun i -> 10 * List.head i + List.last i)
+
+let lol (line:string) =
+  line
+  // configing training ligature protection
+    .Replace("one", "onee") // oneight -> oneeight, etc...
+    .Replace("three", "three")
+    .Replace("nine", "ninee")
+    .Replace("eight", "eightt")
+    // all to text, probably redundant
+    //.Replace("1","one")
+    //.Replace("2","two")
+    //.Replace("3","three")
+    //.Replace("4","four")
+    //.Replace("5","five")
+    //.Replace("6","six")
+    //.Replace("7","seven")
+    //.Replace("8","eight")
+    //.Replace("9","nine")
+    // back to digits
+    .Replace("one","1")
+    .Replace("two","2")
+    .Replace("three","3")
+    .Replace("four","4")
+    .Replace("five","5")
+    .Replace("six","6")
+    .Replace("seven","7")
+    .Replace("eight","8")
+    .Replace("nine","9")
+  |> (fun (s:string) -> 
+           s.ToCharArray() 
+           |> Seq.where Char.IsDigit 
+           |> Seq.map (string >> Int32.Parse)
+           |> (fun i -> 10 * Seq.head i + Seq.last i))
+
+let answer2  (strings: string list) =
+  let numbers = 
+    strings
+    |> Seq.map evilLigatureParser
+  
   numbers
-  |> List.map List.sum
-  |> Seq.sortDescending
-  |> Seq.take 3
   |> Seq.sum
   |> Ok
 
 type Solver() =
-  inherit SolverBase("Calorie Counting")
+  inherit SolverBase("Trebuchet?!")
   with
     override this.Solve input =
       this.DoSolve parseInput [ answer1; answer2 ] input
+
