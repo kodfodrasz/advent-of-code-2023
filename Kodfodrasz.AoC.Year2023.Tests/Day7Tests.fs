aea03ee101612f8dfe4233694daede9ca3b8d950
Gábor Gergely <ggergely@kodfodrasz.net>
2023-12-07 18:18:10 +0100

    Day 7 Part 2

--- Kodfodrasz.AoC.Year2023.Tests/Day7Tests.fs
+++ Kodfodrasz.AoC.Year2023.Tests/Day7Tests.fs
@@ -34,10 +34,156 @@ let ``Answer 1 for example input`` () =
   test
     <@ Ok 6440L = actual @>
 
+[<Fact>]
+let ``Answer 2 for example input`` () =
+  let input = parseInput exampleInput
+  let actual = Result.bind answer2 input
+  test
+    <@ Ok 5905L = actual @>
 
-//[<Fact>]
-//let ``Answer 2 for example input`` () =
-//  let input = parseInput exampleInput
-//  let actual = Result.bind answer2 input
-//  test
-//    <@ Ok 71503 = actual @>
+let parse2 (s : string) = 
+  let cards = s.ToUpperInvariant().ToCharArray() |> Seq.choose Card2.tryParse |> Seq.toList
+  assert (cards.Length = 5)
+  cards
+
+let permuteCards (s: string) =
+  s.ToCharArray()
+  |> Combinatorics.permutations
+  |> Seq.map System.String
+
+let evalHands2TestHelper isKind input = 
+  input
+  |> Seq.collect permuteCards
+  |> Seq.map (fun s ->
+      let c = parse2 s
+      let y = evalHand2 c
+      (isKind y, y))
+  |> Seq.toList
+
+[<Fact>]
+let ``evalHands2 FiveOfAKind`` () =
+  let inputs = [
+    "KKKKK";
+    "JKKKK";
+    "JJKKK";
+    "JJJKK";
+    "JJJJK";
+    "JJJJJ";
+  ]
+
+  let isFiveOfAKind = function
+    | FiveOfAKind _ -> true
+    | _ -> false
+
+  let res = evalHands2TestHelper isFiveOfAKind inputs
+  test
+    <@ List.forall fst res @>
+
+[<Fact>]
+let ``evalHands2 FourOfAKind`` () =
+  let inputs = [
+    // no joker
+    "KKKK2";
+    // 1 joker
+    "KKKJ2";
+    "KKKJ2";
+    "KKKJ2";
+    // 2 jokers
+    "KKJJ2";
+    // 3 jokers
+    "KJJJ2";
+    // no 4 jokers case -> that makes the hand FiveOfAKind
+  ]
+
+  let isFourOfAKind = function
+    | FourOfAKind _ -> true
+    | _ -> false
+
+  let res = evalHands2TestHelper isFourOfAKind inputs
+  test
+    <@ List.forall fst res @>
+
+[<Fact>]
+let ``evalHands2 FullHouse`` () =
+  let inputs = [
+    "KKAAA";
+    // 1 joker
+    "KKAAJ";
+    // no 2 jokers case -> that makes the hand at least FourOfAKkind 
+    // no 3 jokers -> that makes the hand at least FourOfAKkind 
+    // no 4 jokers case -> that makes the hand FiveOfAKind
+  ]
+
+  let isFullHouse = function
+    | FullHouse _ -> true
+    | _ -> false
+
+  let res = evalHands2TestHelper isFullHouse inputs
+  test
+    <@ List.forall fst res @>
+
+[<Fact>]
+let ``evalHands2 ThreeOfAKind`` () =
+  let inputs = [
+    "KKKAQ";
+    // 1 joker
+    "KQAAJ";
+    // 2 jokers 
+    "KQAJJ";
+    // no 3 jokers -> that makes the hand at least FourOfAKkind
+    // no 4 jokers case -> that makes the hand FiveOfAKind
+  ]
+
+  let isThreeOfAKind = function
+    | ThreeOfAKind _ -> true
+    | _ -> false
+
+  let res = evalHands2TestHelper isThreeOfAKind inputs
+  test
+    <@ List.forall fst res @>
+
+[<Fact>]
+let ``evalHands2 TwoPair`` () =
+  let inputs = [
+    "KKQQA";
+    // no joker -> that makes the hand at least ThreeOfAKind
+  ]
+
+  let isTwoPair = function
+    | TwoPair _ -> true
+    | _ -> false
+
+  let res = evalHands2TestHelper isTwoPair inputs
+  test
+    <@ List.forall fst res @>
+
+[<Fact>]
+let ``evalHands2 OnePair`` () =
+  let inputs = [
+    "KKAQT";
+    // 1 joker
+    "KJAQT";
+  ]
+
+  let isOnePair = function
+    | OnePair _ -> true
+    | _ -> false
+
+  let res = evalHands2TestHelper isOnePair inputs
+  test
+    <@ List.forall fst res @>
+
+[<Fact>]
+let ``evalHands2 HighCard`` () =
+  let inputs = [
+    "KQ234";
+    // no joker -> that makes the hand at least OnePair
+  ]
+
+  let isHighCard = function
+    | HighCard _ -> true
+    | _ -> false
+
+  let res = evalHands2TestHelper isHighCard inputs
+  test
+    <@ List.forall fst res @>
--- Kodfodrasz.AoC.Year2023/Day7.fs
+++ Kodfodrasz.AoC.Year2023/Day7.fs
@@ -3,7 +3,6 @@ module Kodfodrasz.AoC.Year2023.Day7
 open System
 open System.Text.RegularExpressions
 open Kodfodrasz.AoC
-open System.Diagnostics
 
 type Card =
  | Number of int
@@ -21,14 +20,6 @@ type Card =
     | 'T' -> Some Tudjafene
     | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' as c -> Some <| Number (c |> string |> int)
     | _ -> None
-  member this.getChar() = 
-    match this with
-    | Ace -> 'A'
-    | King -> 'K'
-    | Queen -> 'Q'
-    | Jumbo -> 'J'
-    | Tudjafene -> 'T'
-    | Number n ->  int '0' + (int)n |> char
 
 type Hand = 
     | HighCard of Card list
@@ -39,14 +30,9 @@ type Hand =
     | FourOfAKind of Card list
     | FiveOfAKind of Card list
 
-[<DebuggerDisplay("{DebuggerDisplay,nq}")>]
 type CardsWithBid = {
   Cards : Card list
   Bid  : Int64 }
-  with
-    member this.DebuggerDisplay = 
-      let cards = this.Cards |> Seq.map (fun c -> c.getChar |> string) |> String.join ""
-      sprintf "%s %i" cards this.Bid
 
 type HandWithBid = {
   Hand : Hand
@@ -66,17 +52,15 @@ let parseInputLine (input : string) : CardsWithBid option  =
 
 let parseInput = Parse.parsePuzzleInputLines parseInputLine
 
-let allsame = 
-  function 
-  | [] -> true
-  | head :: tail -> tail |> List.forall ((=) head)
-;;
-
-let allunique l = 
-  (l |> Seq.distinct |> Seq.length) = (l |> Seq.length)
-;;
-
 let evalHand (l : Card list)= 
+  let allsame = 
+    function 
+    | [] -> true
+    | head :: tail -> tail |> List.forall ((=) head)
+
+  let allunique l = 
+    (l |> Seq.distinct |> Seq.length) = (l |> Seq.length)
+
   List.sort l
   |> function
     | [ a; b; c; d; e ] when 
@@ -122,8 +106,112 @@ let answer1 (cards : CardsWithBid list) =
   |> Seq.sum
   |> Ok
 
+type Card2 =
+ | Joker
+ | Number of int
+ | Tudjafene
+ | Queen
+ | King
+ | Ace
+  static member tryParse = 
+    function
+    | 'A' -> Some Ace
+    | 'K' -> Some King
+    | 'Q' -> Some Queen
+    | 'J' -> Some Joker
+    | 'T' -> Some Tudjafene
+    | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' as c -> Some <| Number (c |> string |> int)
+    | _ -> None
+
+let cardToCard2 (c:Card) : Card2 =
+   match c with
+   | Card.Number n -> Card2.Number n
+   | Card.Tudjafene -> Card2.Tudjafene
+   | Card.Jumbo -> Card2.Joker
+   | Card.Queen -> Card2.Queen
+   | Card.King -> Card2.King
+   | Card.Ace -> Card2.Ace
+
+type Hand2 = 
+    | HighCard of Card2 list
+    | OnePair of Card2 list
+    | TwoPair of Card2 list
+    | ThreeOfAKind of Card2 list
+    | FullHouse of Card2 list
+    | FourOfAKind of Card2 list
+    | FiveOfAKind of Card2 list
+
+type Cards2WithBid = {
+  Cards : Card2 list
+  Bid  : Int64 }
+
+type Hand2WithBid = {
+  Hand : Hand2
+  Bid  : Int64 }
+
+let evalHand2 (l : Card2 list)= 
+  let allsameOrJoker l = 
+    List.groupBy id l
+    |> function 
+    | [] -> true
+    | [_] -> true
+    | [ (k1,_); (k2,_); ] when k1 = Joker || k2 = Joker -> true
+    | _ -> false
+
+  List.sortDescending l // Joker is at list end!
+  |> function
+    | [ a; b; c; d; e ] when 
+        allsameOrJoker [ a ; b; c; d; e] 
+        -> FiveOfAKind l
+    | [ a; b; c; d; e ] when 
+        allsameOrJoker [ a ; b; c; d ] 
+        || allsameOrJoker [ b; c; d; e ] 
+        || allsameOrJoker [ a ; b; c; e ] // extra joker case
+        || allsameOrJoker [ a ; b; d; e ] // extra joker case
+        || allsameOrJoker [ a ; c; d; e ] // extra joker case
+        -> FourOfAKind l 
+    | [ a; b; c; d; e ] when 
+        (allsameOrJoker [ a ; b; c ] && allsameOrJoker [ d; e ])
+        || (allsameOrJoker [a ; b ] && allsameOrJoker [ c; d; e ]) 
+        || (allsameOrJoker [a ; b; e] && allsameOrJoker [ c; d ]) // extra joker case
+        -> FullHouse l 
+    | [ a; b; c; d; e ] when 
+        allsameOrJoker [ a ; b; c ] 
+        || allsameOrJoker [ b ; c; d ] 
+        || allsameOrJoker [ c; d; e]
+        || (allsameOrJoker [ a; d; e]) // extra joker case
+        || (allsameOrJoker [ a; b; e]) // extra joker case
+        || (allsameOrJoker [ b; d; e]) // extra joker case
+        || (allsameOrJoker [ b; c; e]) // extra joker case
+        -> ThreeOfAKind l 
+    | [ a; b; c; d; e ] when 
+        (allsameOrJoker [ a ; b; ] && (allsameOrJoker [ c; d ] || allsameOrJoker [ d; e ] (* extra joker case -> *) || allsameOrJoker [ c; e ] ))
+        || (allsameOrJoker [ b ; c; ] && allsameOrJoker [ d; e ])
+        || (allsameOrJoker [ a ; e; ] && (allsameOrJoker [ b; c ] || allsameOrJoker [ c; d ] || allsameOrJoker [ b; d ])) // extra joker case
+        -> TwoPair l 
+    | [ a; b; c; d; e ] when 
+        (allsameOrJoker [ a; b ] )
+        || (allsameOrJoker [ b; c ]) 
+        || (allsameOrJoker [ c; d ])
+        || (allsameOrJoker [ d; e ])
+        || (allsameOrJoker [ a; e ]) // extra joker case
+        || (allsameOrJoker [ b; e ]) // extra joker case
+        || (allsameOrJoker [ c; e ]) // extra joker case
+        -> OnePair l 
+    | [ _ ; _; _; _; _ ]  -> HighCard l 
+    | _ -> failwith "Unexpected number of cards"
+
 let answer2 (cards : CardsWithBid list) =
-  failwith "TODO"
+  cards
+  |> Seq.map (fun cb -> { Cards = (List.map cardToCard2 cb.Cards); Bid = cb.Bid } )
+  |> Seq.map (fun cb -> {
+      Hand = evalHand2 cb.Cards;
+      Bid = cb.Bid})
+  |> Seq.sortBy (fun hb -> hb.Hand)
+  |> Seq.toList
+  |> Seq.mapi (fun i0 hb -> ((int64)i0 + 1L) * hb.Bid)
+  |> Seq.sum
+  |> Ok
 
 type Solver() =
   inherit SolverBase("Camel Cards")
