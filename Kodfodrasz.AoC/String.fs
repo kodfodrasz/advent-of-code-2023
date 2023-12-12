f3cf6da8b7655f5ac719320d94ef10de6020839b
Gábor Gergely <ggergely@kodfodrasz.net>
2023-12-12 20:38:19 +0100

    Day 10 Part 2

--- Kodfodrasz.AoC/String.fs
+++ Kodfodrasz.AoC/String.fs
@@ -17,3 +17,10 @@ let join (separator: string) (seq: string seq) = System.String.Join(separator, s
 let split separator (str: string) = str.Split separator
 
 let replace (a: string) (b: string) (str: string) = str.Replace(a, b)
+
+/// Transliterate a set of characters to a different set much like the UNIX tr(1) tool
+let tr (fromSymbols:string) (toSymbols:string) (input:string) =
+  // TODO: ensure same length
+  (fromSymbols.ToCharArray(), toSymbols.ToCharArray())
+  ||> Array.zip
+  |> Array.fold (fun (str:string) (a,b) -> str.Replace(a,b)) input
